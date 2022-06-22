f7d5b1334d3402f3ce331465bed2bb2a4cf07747
Semiatom <xmingert@yandex.ru>
2022-06-22 15:26:54 +0300

    Ready for check

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,24 +3,27 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import javax.validation.Valid;
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     private final UserService userService;
+    private final RoleRepository roleRepository;
 
     @Autowired
-    public AdminController(UserService userService) {
+    public AdminController(UserService userService, RoleRepository roleRepository) {
         this.userService = userService;
+        this.roleRepository = roleRepository;
     }
 
     @GetMapping()
@@ -30,7 +33,9 @@ public class AdminController {
     }
 
     @GetMapping(value = "/add")
-    public String createUserForm(User user) {
+    public String createUserForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("allRoles", roleRepository.findAll());
         return "user_create";
     }
 
@@ -40,12 +45,13 @@ public class AdminController {
             return "user_create";
         }
         userService.saveUser(user);
-        return "redirect:/users";
+        return "redirect:/admin";
     }
 
     @GetMapping("/edit/{id}")
     public String editUser(@PathVariable("id") int id, ModelMap model) {
         model.addAttribute("user", this.userService.getUserById(id));
+        model.addAttribute("allRoles", roleRepository.findAll());
         return "user_edit";
     }
 
@@ -55,13 +61,13 @@ public class AdminController {
             return "user_create";
         }
         userService.saveUser(user);
-        return "redirect:/users";
+        return "redirect:/admin";
     }
 
     @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") int id) {
         userService.deleteUser(id);
-        return "redirect:/users";
+        return "redirect:/admin";
     }
 
 
